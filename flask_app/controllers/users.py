455ff95296f68155e6e8b20b284549f9114a08e2
TonyB <tonybaromagic@gmail.com>
2022-06-16 19:11:58 -0400

    sofar

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,35 @@
+from flask_app import app
+from flask import render_template, redirect, request, session, flash
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+from flask_app.models.user import User
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register', methods=["POST"])
+def register():
+    data = {
+            'first_name':request.form['first_name'],
+            'last_name':request.form['last_name'],
+            'email':request.form['email'],
+            'password':request.form['password'],
+            'confirm':request.form['confirm']
+        }
+    if User.validate(data) == True:
+        pw_hash = bcrypt.generate_password_hash(request.form['password'])
+        data.update({'password':pw_hash})
+        User.add_user(data)
+        return ("success")
+    else:
+        return redirect('/')
+
+@app.route('/log_in', methods=["POST"])
+def log_in():
+    data={
+        'email':request.form['email']
+    }
+    User.log_in(data)
+    if bcrypt.check_password_hash(user.password, request.form['password']):
+        print("succsess")
---
+++ flask_app/models/user.py
@@ -0,0 +1,46 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+
+class User:
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def add_user(cls,data):
+        query="INSERT INTO users(first_name,last_name,email,password) VALUES (%(first_name)s,%(last_name)s,%(email)s,%(password)s)"
+        return connectToMySQL('users').query_db( query, data)
+
+    @classmethod
+    def validate(cls,data):
+        valid = True
+        query="SELECT * FROM users WHERE email = %(email)s"
+        results = connectToMySQL('users').query_db( query, data )
+        if len(results) >= 1:
+            flash("Email already taken.","register")
+            valid = False
+        if len(data['first_name']) < 3:
+            flash("First name must be at least 3 characters","register")
+            valid =  False
+        if len(data['last_name']) < 3:
+            flash("Last name must be at least 3 characters","register")
+            valid =  False
+        if len(data['password']) < 8:
+            flash("Password must be at least 8 characters","register")
+            valid =  False
+        if data['password'] != data['confirm']:
+            flash("Passwords don't match","register")
+            valid = False
+        return valid
+
+    @classmethod
+    def log_in(cls,data):
+        query="SELECT * FROM users WHERE email = %(email)s"
+        results = connectToMySQL('users').query_db( query, data )
+        return User(results[0])
+        
